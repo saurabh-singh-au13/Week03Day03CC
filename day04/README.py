c384d52c7e9a9aec654f101622fc99f0e75d210c
saurabh <saurabh@gmail.com>
2020-09-11 07:44:00 -0400

    day05cc work

--- day04/README.py
+++
@@ -1,4 +0,0 @@
-num=int(input("Enter the no. to find table :"))
-for i in range(1, 11):
-    print(num, 'x',i, '=', num*i)
-    
--- day04/vowel.py
+++
@@ -1,4 +0,0 @@
-for i in range(0, 1):
-    for j in range(0, i):
-        print(j)
-        
